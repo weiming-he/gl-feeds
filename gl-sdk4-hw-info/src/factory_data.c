81a388e711567139691793eda3f7c5da3c791fa2
Jianhui Zhao <jianhui.zhao@gl-inet.com>
2024-05-13 12:08:40 +0800

    perf(gl-sdk4-hwinfo): optimize code
    Signed-off-by: Jianhui Zhao <jianhui.zhao@gl-inet.com>

--- gl-sdk4-hw-info/src/factory_data.c
+++ gl-sdk4-hw-info/src/factory_data.c
@@ -58,14 +58,33 @@ static int block_part_read(const char *part, unsigned int from,
     return 0;
 }
 
+static int parse_mtd_value(const char *part, u32 offset, void *dest, int len)
+{
 #ifdef CONFIG_MTD
-static int parse_mtd_value(struct device_node *np, const char *prop,
+    struct mtd_info *mtd;
+    size_t retlen;
+    int rc;
+
+    mtd = get_mtd_device_nm(part);
+    if (IS_ERR(mtd))
+        return -1;
+
+    rc = mtd_read(mtd, offset, len, &retlen, dest);
+    if (!rc && retlen != len)
+        rc = -EIO;
+
+    put_mtd_device(mtd);
+    return rc;
+#else
+    return -ENODEV;
+#endif
+}
+
+static int parse_value(struct device_node *np, const char *prop,
                            void *dest, int len)
 {
     const char *part, *offset_str;
-    struct mtd_info *mtd;
     u32 offset = 0;
-    size_t retlen;
 
     if (of_property_read_string_index(np, prop, 0, &part))
         return -1;
@@ -73,87 +92,68 @@ static int parse_mtd_value(struct device_node *np, const char *prop,
     if (!of_property_read_string_index(np, prop, 1, &offset_str))
         offset = simple_strtoul(offset_str, NULL, 0);
 
-    if(!strncmp(part,"/dev/mmc",8))
+    if(!strncmp(part,"/dev/mmc", 8))
         return block_part_read(part, offset, dest, len);
 
-    mtd = get_mtd_device_nm(part);
-    if (IS_ERR(mtd))
-        return -1;
-
-    mtd_read(mtd, offset, len, &retlen, dest);
-    put_mtd_device(mtd);
-
-    return 0;
+    return parse_mtd_value(part, offset, dest, len);
 }
-#endif
 
 static void make_device_country_code(struct device_node *np)
 {
-#ifdef CONFIG_MTD
     __be16 country_code = 0;
 
-    if (parse_mtd_value(np, "country_code", &country_code, COUNTRY_LEN))
+    if (parse_value(np, "country_code", &country_code, COUNTRY_LEN))
         return;
 
     create_proc_node("country_code", lookup_country(be16_to_cpu(country_code)));
-#endif
 }
 
 static void make_device_sn_bak(struct device_node *np)
 {
-#ifdef CONFIG_MTD
-    if (parse_mtd_value(np, "device_sn_bak", gl_hw_info.device_sn_bak, SN_LEN))
+    if (parse_value(np, "device_sn_bak", gl_hw_info.device_sn_bak, SN_LEN))
         return;
 
     if (strlen(gl_hw_info.device_sn_bak))
         create_proc_node("device_sn_bak", gl_hw_info.device_sn_bak);
-#endif
 }
 
 static void make_device_sn(struct device_node *np)
 {
-#ifdef CONFIG_MTD
-    if (parse_mtd_value(np, "device_sn", gl_hw_info.device_sn, SN_LEN))
+    if (parse_value(np, "device_sn", gl_hw_info.device_sn, SN_LEN))
         return;
 
     if (strlen(gl_hw_info.device_sn))
         create_proc_node("device_sn", gl_hw_info.device_sn);
-#endif
 }
 
 static void make_device_ddns(struct device_node *np)
 {
-#ifdef CONFIG_MTD
-    if (parse_mtd_value(np, "device_ddns", gl_hw_info.device_ddns, DDNS_LEN))
+    if (parse_value(np, "device_ddns", gl_hw_info.device_ddns, DDNS_LEN))
         return;
 
     if (strlen(gl_hw_info.device_ddns))
         create_proc_node("device_ddns", gl_hw_info.device_ddns);
-#endif
 }
 
 static void make_device_mac(struct device_node *np)
 {
-#ifdef CONFIG_MTD
     u8 mac[ETH_ALEN];
 
-    if (parse_mtd_value(np, "device_mac", mac, MAC_LEN))
+    if (parse_value(np, "device_mac", mac, MAC_LEN))
         return;
 
     if (is_valid_ether_addr(mac)) {
         snprintf(gl_hw_info.device_mac, sizeof(gl_hw_info.device_mac), "%pM", mac);
         create_proc_node("device_mac", gl_hw_info.device_mac);
     }
-#endif
 }
 
 static void make_device_cert(struct device_node *np)
 {
-#ifdef CONFIG_MTD
     size_t key_len;
     char *p;
 
-    if (parse_mtd_value(np, "device_cert", gl_hw_info.device_cert, CERT_LEN))
+    if (parse_value(np, "device_cert", gl_hw_info.device_cert, CERT_LEN))
         return;
 
     if (strlen(gl_hw_info.device_cert) == 0)
@@ -179,18 +179,16 @@ static void make_device_cert(struct device_node *np)
 
     create_proc_node("device_cert", gl_hw_info.device_cert);
     create_proc_node("device_key", gl_hw_info.device_key);
-#endif
 }
 
 static void make_device_submodel(struct device_node *np)
 {
-#ifdef CONFIG_MTD
     size_t submodel_len;
     size_t i;
     bool all_ff = true;
     char *p;
 
-    if (parse_mtd_value(np, "device_submodel", gl_hw_info.device_submodel, SUBMODEL_LEN))
+    if (parse_value(np, "device_submodel", gl_hw_info.device_submodel, SUBMODEL_LEN))
         return;
 
     p = gl_hw_info.device_submodel;
@@ -217,7 +215,6 @@ static void make_device_submodel(struct device_node *np)
     }
 
     create_proc_node("device_submodel", gl_hw_info.device_submodel);
-#endif
 }
 
 void make_factory_data(struct device_node *np)
